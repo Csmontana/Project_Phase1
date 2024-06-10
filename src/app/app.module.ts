d5c7c9271c2a763539b532633c596afa60d8c876
Cristina <cristinascomputer@Cristinas-Air.lan>
2024-06-10 01:43:42 -0700

    Have a Client Form that is Routing with the other web pages

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClientModule } from './client.module'; // Ensure this path is correct
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/client.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClientFormComponent } from './client-form/client-form.component';
+import { MeetingComponent } from './meeting/meeting.component';
+
+@NgModule({
+  declarations: [
+    ClientFormComponent,
+    MeetingComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ClientFormComponent,
+    MeetingComponent
+  ]
+})
+export class ClientModule {}
